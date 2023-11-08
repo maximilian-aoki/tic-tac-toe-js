4c419a2c24c70cff2fa55c434b50095c3d51c775
Maximilian Aoki <135271252+maximilian-aoki@users.noreply.github.com>
2023-11-08 10:53:19 -0500

    Set UI Components

---
+++ js/events.js
@@ -0,0 +1,32 @@
+let events = (function() {
+  let events = {};
+  
+  // create a new function for a new or existing event name (doesn’t check duplicates) 
+  function on(eventName, fn) {
+    events[eventName] = events[eventName] || [];
+    events[eventName].push(fn);
+  };
+  
+  // remove an event name’s associated function (one instance in this e.g.)
+  function off(eventName, fn) {
+    if (events[eventName]) {
+      for (var i = 0; i < events[eventName].length; i++) {
+        if (events[eventName][i] === fn) {
+          events[eventName].splice(i, 1);
+          break;
+        }
+      }
+    }
+  };
+  
+  // emit a piece of data to all functions on one event name
+  function emit(eventName, data) {
+    if (events[eventName]) {
+      events[eventName].forEach((fn) => {
+        fn(data);
+      });
+    }
+  };
+  
+  return {on, off, emit};
+})();
